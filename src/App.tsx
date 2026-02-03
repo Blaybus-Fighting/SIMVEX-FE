c54334522ad4c9b10b7a6bc88a6674049e032a24
TaekkiMin <tktwin00@gmail.com>
2026-02-03 17:11:13 +0900

    refactor: 불필요한 div 태그 제거

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <>
       <Routes>
         {/* 라우팅 설정을 위해 임시로 만들어놓은 페이지 */}
         <Route path="/" element={<Home />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
