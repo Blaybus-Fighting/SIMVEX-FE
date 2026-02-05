73bc0b3748dab2cf49561b52c365eb35aad9d997
Alal11 <fingertime00@gmail.com>
2026-02-05 10:34:53 +0900

    style: 탭 간격 2칸으로 수정 #11

--- src/api/queryClient.ts
+++ src/api/queryClient.ts
@@ -4,11 +4,11 @@
 import { QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            retry: 1, // API 실패시 1번만 재시도
-            staleTime: 1000 * 60 * 5, // 5분 동안은 캐시된 데이터 사용 (서버 요청 안 함)
-            refetchOnWindowFocus: false, // 윈도우 포커스 될 때마다 재요청 금지
-        },
+  defaultOptions: {
+    queries: {
+      retry: 1, // API 실패시 1번만 재시도
+      staleTime: 1000 * 60 * 5, // 5분 동안은 캐시된 데이터 사용 (서버 요청 안 함)
+      refetchOnWindowFocus: false, // 윈도우 포커스 될 때마다 재요청 금지
     },
+  },
 });
