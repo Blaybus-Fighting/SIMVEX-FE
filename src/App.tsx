a3a90d852dbb763d928251a0710920f72bdada5f
마정미 <jungmee0807@gmail.com>
2026-02-10 03:38:10 +0900

    Revert "feat: quiz page UI and modal with animated gear character"
    This reverts commit d9fe7cda3e7f70bec86293c1a85ee326ccbbdcb4.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-
-import LandingPage from "./pages/LandingPage";
-import StudyPage from "./pages/StudyPage";
+import StudyPage from "@pages/StudyPage.tsx";
+import LandingPage from "@/pages/LandingPage";
 import AssetPage from "./pages/AssetPage";
-import QuizPage from "./pages/QuizPage";
 
 function App() {
   return (
     <div className="h-full">
       <Routes>
-        {/* 기본 */}
+        {/* / 경로로 들어와도 /home으로 보내기 */}
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<LandingPage />} />
-
-        {/* 학습 */}
         <Route path="/study" element={<StudyPage />} />
-
-        {/* 퀴즈 */}
-        <Route path="/quiz" element={<QuizPage />} />
-
-        {/* 기타 */}
         <Route path="/asset" element={<AssetPage />} />
       </Routes>
     </div>
