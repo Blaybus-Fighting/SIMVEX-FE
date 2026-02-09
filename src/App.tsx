d9fe7cda3e7f70bec86293c1a85ee326ccbbdcb4
마정미 <jungmee0807@gmail.com>
2026-02-10 03:27:51 +0900

    feat: quiz page UI and modal with animated gear character

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import StudyPage from "@pages/StudyPage.tsx";
-import LandingPage from "@/pages/LandingPage";
+
+import LandingPage from "./pages/LandingPage";
+import StudyPage from "./pages/StudyPage";
 import AssetPage from "./pages/AssetPage";
+import QuizPage from "./pages/QuizPage";
 
 function App() {
   return (
     <div className="h-full">
       <Routes>
-        {/* / 경로로 들어와도 /home으로 보내기 */}
+        {/* 기본 */}
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<LandingPage />} />
+
+        {/* 학습 */}
         <Route path="/study" element={<StudyPage />} />
+
+        {/* 퀴즈 */}
+        <Route path="/quiz" element={<QuizPage />} />
+
+        {/* 기타 */}
         <Route path="/asset" element={<AssetPage />} />
       </Routes>
     </div>
