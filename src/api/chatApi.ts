d87d6af94c0c1fdae489be2e43ab08760bd268ce
Alal11 <fingertime00@gmail.com>
2026-02-09 16:54:42 +0900

    feat: .env에 맞는 API 엔드포인트 수정 #30

--- src/api/chatApi.ts
+++ src/api/chatApi.ts
@@ -4,9 +4,8 @@ import type { ChatListResponse } from "@/types/chat";
 // 채팅 내역 조회 함수
 export const getChatHistory = async (sessionId: number, page: number = 0) => {
 
-  // 예: sessionId가 3이면 -> GET /api/chat/3/messages?page=0 요청이 나감
   const response = await api.get<ChatListResponse>(
-    `/chat/${sessionId}/messages?page=${page}`
+    `/api/chat/${sessionId}/messages?page=${page}`
   );
 
   return response.data;
